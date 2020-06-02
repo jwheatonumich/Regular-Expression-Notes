3af408d08bd69de575808a459828dd37863c3ccf
jwheaton <jwheatonumich@gmail.com>
2020-06-01 20:18:12 -0500

    Update Regular-Expressions-Notes-1.js

--- Regular-Expressions-Notes-1.js
+++ Regular-Expressions-Notes-1.js
@@ -23,3 +23,34 @@ let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /Twinkle/gi; //Uses both g and i to get multiple matches and ignore case
 let result = twinkleStar.match(starRegex);
 
+//The "." character is a wildcard
+let humStr = "I'll hum a song";
+let hugStr = "Bear hug";
+let huRegex = /hu./;
+huRegex.test(humStr); // Returns true
+huRegex.test(hugStr); // Returns true
+
+//Hard brackets "[]" define a set of characters that are acceptable
+let bigStr = "big";
+let bagStr = "bag";
+let bugStr = "bug";
+let bogStr = "bog";
+let bgRegex = /b[aiu]g/;
+bigStr.match(bgRegex); // Returns ["big"]
+bagStr.match(bgRegex); // Returns ["bag"]
+bugStr.match(bgRegex); // Returns ["bug"]
+bogStr.match(bgRegex); // Returns null
+
+//A hyphen can be used to match anything between two latters "[a-e]"
+let catStr = "cat";
+let batStr = "bat";
+let matStr = "mat";
+let bgRegex = /[a-e]at/;
+catStr.match(bgRegex); // Returns ["cat"]
+batStr.match(bgRegex); // Returns ["bat"]
+matStr.match(bgRegex); // Returns null
+
+//Hyphens can also be used for numbers "[0-5]""
+let jennyStr = "Jenny8675309";
+let myRegex = /[a-z0-9]/ig; // matches all letters and numbers in jennyStr
+jennyStr.match(myRegex);
